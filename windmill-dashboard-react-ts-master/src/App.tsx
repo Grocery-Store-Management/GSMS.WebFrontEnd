abe8db571c1ec14699d6a0f82dbcf1604e57ab4b
JackWH <yukiakira259@gmail.com>
2022-02-09 15:40:26 +0700

    Update App.tsx

--- windmill-dashboard-react-ts-master/src/App.tsx
+++ windmill-dashboard-react-ts-master/src/App.tsx
@@ -1,4 +1,4 @@
-import { lazy } from 'react'aaa
+import { lazy } from 'react'
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 import AccessibleNavigationAnnouncer from './components/AccessibleNavigationAnnouncer'
 import React from 'react';
